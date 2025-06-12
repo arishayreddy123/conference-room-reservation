ffbeca0d1de36508464d123de2667ebff662ce51
Arishay <arishay.r16@gmail.com>
2025-06-13 10:48:36 +1200

    Created custom user model with admin and regular user roles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_regular_user = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
