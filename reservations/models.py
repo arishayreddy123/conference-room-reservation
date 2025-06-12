80884079b4cad91e878b2228902ea86f8e621b04
Arishay <arishay.r16@gmail.com>
2025-06-13 10:57:38 +1200

    Created Reservation model with user, room, date, time

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from rooms.models import Room
 
-# Create your models here.
+class Reservation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.room.name} on {self.date}"
